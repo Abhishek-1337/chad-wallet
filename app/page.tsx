9db6e94a4e35cd9d9735ab272c8e3105a14aa37b
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-06-28 21:21:19 +0530

    rearrange the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/shared/Navbar";
+import AuthRedirect from "@/components/shared/AuthRedirect";
 import TokenBanner from "@/components/landing/TokenBanner";
 import Hero from "@/components/landing/Hero";
 import AvailableOnWeb from "@/components/landing/AvailableOnWeb";
@@ -10,15 +11,16 @@ import Footer from "@/components/landing/Footer";
 export default function Home() {
   return (
     <div className="relative isolate flex min-h-svh flex-col overflow-x-hidden">
+      <AuthRedirect />
       <Navbar />
       <main className="flex w-full flex-1 flex-col items-center justify-center pt-13">
-        {/* <TokenBanner direction="left" /> */}
-        <Hero />
         <TokenBanner direction="right" />
+        <Hero />
         <AvailableOnWeb />
         <Features />
-        {/* <TrendingGrid /> */}
+        <TrendingGrid />
         <CtaBottom />
+        <TokenBanner direction="right" />
       </main>
       <Footer />
     </div>
